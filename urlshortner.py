21854a601daa3de39dd6dc392060b450ec9ef37c
Yeswanth Gunisetty <123174008+yeswanth3399@users.noreply.github.com>
2023-06-02 10:36:22 +0530

    Add files via upload

---
+++ urlshortner.py
@@ -0,0 +1,14 @@
+#pip install pyshorteners
+#pip install pyperclip
+
+import pyshorteners
+
+url=input('Enter the url: ')
+
+def shortenurl(url):
+    s=pyshorteners.Shortener()
+    print('Shorten Url is :')
+    print(s.tinyurl.short(url))
+
+
+shortenurl(url)
